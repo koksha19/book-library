d176ebb6b2a9707ced0509c1515397b64f9a2bfa
Lev <lev.bereza@gmail.com>
2024-11-21 12:39:29 +0200

    Connecting to MongoDB database using Mongoose

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const connectDb = require("./config/connectDb");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+connectDb();
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.post('/', (req, res) => {
+  res.send("Respond to POST request");
 });
+
+mongoose.connection.once('open', () => {
+  console.log("Connected to the database");
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+})
+
+
